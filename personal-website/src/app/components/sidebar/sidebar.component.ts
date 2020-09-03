e9cb705ff13268969a979d167c486fd25c75a019
José Franco <josepgvf@gmail.com>
2020-09-03 22:31:31 +0100

    feat(sidebar): added sidebar animation, views and logic

---
+++ personal-website/src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+
+  isExpanded = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  /**
+   * Expands the side bar
+   */
+  expandSidebar(): void {
+    this.isExpanded = true;
+  }
+
+  /**
+   * Collapses the side bar
+   */
+  collapseSidebar(): void {
+    this.isExpanded = false;
+  }
+
+}
